4c9c8d892b4fecd29b2907eb4e38ebc9fdc5f8a4
Jason Karlavige <jkarlavige@gmail.com>
2019-09-01 07:10:42 -0400

    added heroku ports

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 // Connect to database
 const db = require('./config/keys').mongoURI;
@@ -27,6 +28,15 @@ require('./config/passport')(passport);
 const routes = require('./routes/api/');
 // Use routes
 app.use('/api/', routes);
+
+// Serve static assets
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Set port for Heroku, else use local 5000
 const port = process.env.PORT || 5000;
